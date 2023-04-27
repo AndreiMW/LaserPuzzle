fa53759b870c3a975544a078c494c3509636d873
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-27 16:44:57 +0300

    Added logic to check if should shoot lasers and method to reset lasers

--- Assets/Scripts/Managers/LaserManager.cs
+++ Assets/Scripts/Managers/LaserManager.cs
@@ -23,6 +23,8 @@ public class LaserManager : MonoBehaviour {
 
 	private int _maxLaserDistance = 100;
 
+	private bool _shootLasers = false;
+
 	#region Lifecycle
 	
 	private void Awake() {
@@ -30,15 +32,41 @@ public class LaserManager : MonoBehaviour {
 	}
 
 	private void FixedUpdate() {
-		int linesCount = 0;
+		if (this._shootLasers) {
+			int linesCount = 0;
 
-		linesCount += this.CalculateLaserLine(this._laserOrigin.Info, linesCount);
+			linesCount += this.CalculateLaserLine(this._laserOrigin.Info, linesCount);
 		
-		this.RemoveOldLasers(linesCount);
+			this.RemoveOldLasers(linesCount);	
+		}
 	}
 
 	private void OnDestroy() {
 		s_instance = null;
+	}
+	
+	#endregion
+	
+	#region Public
+
+	/// <summary>
+	/// Set if the laser origin shoudlt shoot lasers.
+	/// </summary>
+	/// <param name="canShoot">True if should shoot laser, false otherwise. </param>
+	public void ChangeCanShootLaser(bool canShoot) {
+		this._shootLasers = canShoot;
+	}
+
+	/// <summary>
+	/// Reset the lasers. (Delete current visible lasers)
+	/// </summary>
+	public void ResetLasers() {
+		if (this._visibleLines is null) {
+			return;
+		}
+		for (int i = 0; i < this._visibleLines.Count; i++) {
+			this.RemoveOldLasers(i);
+		}
 	}
 	
 	#endregion
@@ -60,23 +88,34 @@ public class LaserManager : MonoBehaviour {
 		Vector3 hitPos = hit.point;
 		LaserInfo nextInfo = info;
 		if (raycast) {
-			if (hit.collider.CompareTag(Tags.MIRROR)) {
-				nextInfo.StartPosition = hitPos;
-				nextInfo.Direction = Vector3.Reflect(info.Direction, hit.normal);
-				nextInfo.Color = info.Color;
+			switch (hit.collider.tag) {
+				case Tags.MIRROR: {
+					
+					nextInfo.StartPosition = hitPos;
+					nextInfo.Direction = Vector3.Reflect(info.Direction, hit.normal);
+					nextInfo.Color = info.Color;
 				
-				result += this.CalculateLaserLine(nextInfo, index + result);
-			}else if (hit.collider.CompareTag(Tags.FILTER)) {
-				Color filterColor = hit.collider.GetComponent<FilterColor>().Color;
-				if (info.Color != Color.white) {
-					//TODO Refactor this, it's not working right
-					filterColor = (filterColor + info.Color) / 2;
+					result += this.CalculateLaserLine(nextInfo, index + result);
+					
+					break;
 				}
-				nextInfo.StartPosition = hit.collider.transform.position;
-				nextInfo.Color = filterColor;
-				nextInfo.Direction = info.Direction;
+				case Tags.FILTER: {
+					Color filterColor = hit.collider.GetComponent<FilterColor>().Color;
+					if (info.Color != Color.white) {
+						//TODO Refactor this, it's not working right
+						filterColor = (filterColor + info.Color) / 2;
+					}
+					nextInfo.StartPosition = hit.collider.transform.position;
+					nextInfo.Color = filterColor;
+					nextInfo.Direction = info.Direction;
 				
-				result += this.CalculateLaserLine(nextInfo, index + result);
+					result += this.CalculateLaserLine(nextInfo, index + result);
+					break;
+				}
+				case Tags.LEVEL_END: {
+					GameManager.Instance.ChangeState(GameManager.Instance.GameEnd);
+					break;
+				}
 			}
 		} else {
 			hitPos = info.StartPosition + info.Direction * this._maxLaserDistance;
