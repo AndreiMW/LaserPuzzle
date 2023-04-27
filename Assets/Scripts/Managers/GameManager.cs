b9234117be34e1e682cecaf4fad4b663f14641ca
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-27 16:44:18 +0300

    Added basic implementation for game manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,47 @@
+/**
+ * Created Date: 4/27/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Avangarde Software. All rights reserved. 
+ */
+
+using GameState;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	private static GameManager s_instance;
+	public static GameManager Instance => s_instance ??= FindObjectOfType<GameManager>();
+	
+	public readonly GameIdleState GameIdle = new ();
+	public readonly GameInProgressState GameInProgress = new ();
+	public readonly GameEndState GameEnd = new ();
+
+	private BaseGameState _currentState;
+	
+	#region Lifecycle
+
+	private void Awake() {
+		this._currentState = this.GameIdle;
+		this._currentState.OnStateEnter(this);
+	}
+
+	private void Update() {
+		this._currentState.OnUpdate(this);
+	}
+	
+	#endregion
+	
+	#region Public
+
+	/// <summary>
+	/// Change the game's state.
+	/// </summary>
+	/// <param name="state">The state to change to.</param>
+	public void ChangeState(BaseGameState state) {
+		this._currentState.OnStateExit(this);
+		this._currentState = state;
+		this._currentState.OnStateEnter(this);
+	}
+	
+	#endregion
+}
