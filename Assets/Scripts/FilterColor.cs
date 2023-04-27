3505c7181a6a22948aa162364e28c9701522f577
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-27 15:11:45 +0300

    Added basic implementation for filter color

---
+++ Assets/Scripts/FilterColor.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+/**
+ * Created Date: 4/25/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Avangarde Software. All rights reserved. 
+ */
+
+public class FilterColor : MonoBehaviour {
+	[field:SerializeField]
+	public Color Color { get; private set; }
+}
