7b187b594475015d8683d738e64c25ba43ed816f
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-27 15:12:08 +0300

    Added Tags static class

---
+++ Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+/**
+ * Created Date: 4/27/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Avangarde Software. All rights reserved. 
+ */
+
+public static class Tags {
+	public const string MIRROR = "Mirror";
+	public const string FILTER = "Filter";
+}
