4e91d2825088a130edbb8aea392ee4707c57d103
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-04-28 11:59:22 +0300

    Added implementation for LevelData scriptable object

---
+++ Assets/Scripts/Levels/LevelData.cs
@@ -0,0 +1,20 @@
+/**
+ * Created Date: 4/28/2023
+ * Author: Andrei-Florin Ciobanu
+ * 
+ * Copyright (c) 2023 Avangarde Software. All rights reserved. 
+ */
+
+using UnityEngine;
+
+namespace Levels {
+	[CreateAssetMenu(fileName = "Level000", menuName = "Level/CreateLevelData")]
+	public class LevelData : ScriptableObject {
+ 
+		[field : SerializeField] 
+		public Level LevelPrefab {get; private set;} 
+		
+		[field:SerializeField]
+		public Color EndLevelColor { get; private set; }
+	}
+}
