2ca1c084d62f285fb57b5170db9f111cf6b1e62b
Andrei-Florin Ciobanu <andrei.ciobanu@avangarde-software.com>
2023-05-03 17:08:18 +0300

    Added logic to update filter material to color

--- Assets/Scripts/FilterColor.cs
+++ Assets/Scripts/FilterColor.cs
@@ -1,5 +1,3 @@
-using UnityEngine;
-
 /**
  * Created Date: 4/25/2023
  * Author: Andrei-Florin Ciobanu
@@ -7,7 +5,62 @@ using UnityEngine;
  * Copyright (c) 2023 Avangarde Software. All rights reserved. 
  */
 
+using UnityEngine;
+
 public class FilterColor : MonoBehaviour {
-	[field:SerializeField]
-	public Color Color { get; private set; }
+	private enum Colors {
+		Magenta,
+		Cyan,
+		Yellow,
+		Green,
+		Red,
+		Blue
+	}
+
+	[SerializeField]
+	private Colors _filterColor;
+
+	private Renderer _renderer;
+
+	public Color32 ColorOfFilter { get; private set; }
+	
+	#region Lifecycle
+
+	private void Awake() {
+		this._renderer = this.GetComponent<Renderer>();
+
+		this._renderer.material.color = this.ColorOfFilter;
+		this.ColorOfFilter = this.ColorFromEnum();
+	}
+	
+	#endregion
+	
+	#region Private
+
+	private Color32 ColorFromEnum() {
+		switch (this._filterColor) {
+			case Colors.Magenta: {
+				return Color.magenta;
+			}
+			case Colors.Cyan: {
+				return Color.cyan;
+			}
+			case Colors.Green: {
+				return Color.green;
+			}
+			case Colors.Yellow: {
+				return Color.yellow;
+			}
+			case Colors.Red: {
+				return Color.red;
+			}
+			case Colors.Blue: {
+				return Color.blue;
+			}
+			default:
+				return Color.white;
+		}
+	}
+	
+	#endregion
 }
